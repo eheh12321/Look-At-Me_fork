6bed66128e945f9e4ca752f6874a401261198cab
eheh12321 <eheh12321@gmail.com>
2023-02-22 15:00:46 +0900

    feat: 댓글 Pagination 추가

--- client/src/pages/PostView.js
+++ client/src/pages/PostView.js
@@ -21,7 +21,6 @@ const PostView = () => {
   const params = useParams();
   const [detailData, setDetailData] = useState([]);
   const [isFollowing, setIsFollowing] = useState(false);
-
   const url = 'https://myprojectsite.shop';
   const name = detailData.member?.nickname;
   const sentId = detailData.member?.memberId;
