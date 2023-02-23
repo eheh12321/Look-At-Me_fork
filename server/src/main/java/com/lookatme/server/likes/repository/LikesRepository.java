0a2783d8219098933a3ea98f5af7eac6431c7e4b
이도형 <eheh12321@gmail.com>
2023-02-23 22:28:53 +0900

    fix: JWT, 페이지네이션 버그 수정 (#13)
    * refactor: Refresh Token 로직 수정 테스트
    
    * refactor: Refresh Token 로직 수정 테스트 - 2
    
    * refactor: Refresh Token 로직 수정 테스트 - 3
    
    * refactor: Refresh Token 로직 수정 테스트 3-2
    
    * refactor: 페이지네이션 적용 이후 좋아요 기능 버그 수정

--- server/src/main/java/com/lookatme/server/likes/repository/LikesRepository.java
+++ server/src/main/java/com/lookatme/server/likes/repository/LikesRepository.java
@@ -8,11 +8,13 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface LikesRepository extends JpaRepository<Likes, Long> {
     Likes findByBoardAndMember(Board board, Member member);
 
-    Page<Likes> findByMember(Member member, Pageable pageable);
+    List<Likes> findByMember(Member member);
 
     boolean existsByBoardAndMember(Board board, Member member);
 }
